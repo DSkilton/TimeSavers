57fe0c86fee9e853bf780a87e98356a59421cb97
Duncan R Skilton <mc03353@mbro.ac.uk>
2021-02-03 20:55:08 +0000

    added notes

--- HelloWorld/readCSV.py
+++ HelloWorld/readCSV.py
@@ -1,9 +1,9 @@
-file = open("gamerscore.csv", "r")
-line = file.readline()
+file = open("gamerscore.csv", "r")#opens file ready to use
+line = file.readline()#stores a line in line
 
 while(line):
-    data = line.split(",")
-    print("Handle: ", data[0])
-    print("Gamescore: ",data[1])
-    line=file.readline()
-file.close()
+    data = line.split(",")#seperates data into array
+    print("Handle: ", data[0])#printing array at index 0
+    print("Gamescore: ",data[1])#printing array at index 1
+    line=file.readline()#reading next line
+file.close()#closing file
