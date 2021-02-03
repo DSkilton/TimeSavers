ca101738a1249c6f794f14fbf10cffea775aefca
Duncan R Skilton <mc03353@mbro.ac.uk>
2021-02-03 20:52:32 +0000

    updated while loop to split csv into array and print elements

--- HelloWorld/readCSV.py
+++ HelloWorld/readCSV.py
@@ -2,7 +2,8 @@ file = open("gamerscore.csv", "r")
 line = file.readline()
 
 while(line):
-    print(line)
+    data = line.split(",")
+    print("Handle: ", data[0])
+    print("Gamescore: ",data[1])
     line=file.readline()
-
 file.close()
