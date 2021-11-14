8606e8345b41d7ad3bbf593522e8b145a12b9a06
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2021-11-14 21:05:59 +0000

    added nest ternary

--- HelloWorld/TernaryOperator.py
+++ HelloWorld/TernaryOperator.py
@@ -1,5 +1,19 @@
+x = 79
 
-def odd_even():
-    x = 0
+def if_to_ternary():
+    if(x % 2 == 0):
+        print('even')
+    else:
+        print('false')
+
+    #Ternary operator
     even = 'even' if x % 2 == 0 else 'false'
     print(even)
+
+
+def if_to_ternary():
+    grade = ('A' if (x >= 90) else ('B' if (x >= 80) else 'C'))
+    print('Grade is ' + str(grade))
+
+
+print(if_to_ternary())
