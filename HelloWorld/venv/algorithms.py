846ce5e8663a76ee5aea350f789dc3c0266fa923
Duncan R Skilton <mc03353@mbro.ac.uk>
2021-11-14 09:59:33 +0000

    added basic algorithm module and match case

---
+++ HelloWorld/venv/algorithms.py
@@ -0,0 +1,23 @@
+a = 5
+b = 6
+c = 10
+n = 10
+
+range(n)
+
+for i in range(n):
+    for j in range(n):
+        x = i * i
+        y = j * j
+        z = i * j
+        print(x)
+        print(y)
+        print(z)
+
+for k in range(n):
+    w = a * k + 45
+    v = b * b
+    print(w)
+    print(v)
+d = 33
+
