2a822db4b2e8f006132300fb11188a5cda16bcf6
Duncan R Skilton <mc03353@mbro.ac.uk>
2021-11-14 10:28:26 +0000

    added list if statement

---
+++ HelloWorld/DataStructures.py
@@ -0,0 +1,7 @@
+names = ['Moritz', 'Billy', 'James', 'Daniel', 'Chloe',
+         'Ben', 'Kayhan', 'Jacob', 'Tyler', 'Alfie']
+
+if 'Ben' in names:
+    print('This conditional evaluated to true')
+
+print('\nThis is outside the loop')
