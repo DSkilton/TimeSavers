aea4e18bf76be51b7646ce934afd7c0199e95d1e
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2021-11-18 21:10:24 +0000

    added linear algorith anagram solution

--- HelloWorld/Anagram.py
+++ HelloWorld/Anagram.py
@@ -29,7 +29,7 @@ def anagram_solution2(s1, s2):
     s1_list = list(s1)
     s2_list = list(s2)
 
-    s1_list.sort()
+    s1_list.sort()# sorting uses either 0(n2) or O(n log n)
     s2_list.sort()
 
     pos = 0
@@ -44,3 +44,28 @@ def anagram_solution2(s1, s2):
     return matches
 
 print(anagram_solution2("cake", "kace"))
+
+def anagram_solution3(s1, s2):
+# This algorithm sacrifices space in order to gain speed
+    c1 = [0] * 26
+    c2 = [0] * 26
+
+    for i in range(len(s1)):
+        pos = ord(s1[i]) - ord('a')
+        c1[pos] = c1[pos] + 1
+
+    for i in range(len(s1)):
+        pos = ord(s2[i]) - ord('a')
+        c2[pos] = c2[pos] + 1
+
+    j = 0
+    still_ok = True
+    while j < 26 and still_ok:
+        if c1[j] == c2[j]:
+            j = j + 1
+        else:
+            still_ok = False
+
+    return still_ok
+
+print(anagram_solution3("cake", "kace"))
