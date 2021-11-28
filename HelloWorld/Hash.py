3866d6e5560589baa8b83c013cd23d9893b1b993
Duncan R Skilton <dr.skilton@mbro.ac.uk>
2021-11-28 20:24:31 +0000

    added random dict key value generator

--- HelloWorld/Hash.py
+++ HelloWorld/Hash.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 
 def tuple_dict():
     tuple_example = ("a", 2, "password", "Password")
@@ -6,4 +9,14 @@ def tuple_dict():
         hash_example = hash(mutable)
         print(hash_example)
 
+def random_string(length):
+    result = ''.join((random.choice(string.ascii_lowercase) for x in range(length)))
+    return result
 
+
+def generate_dict():
+    for i in range(100000):
+        print("'" + random_string(random.randint(1, 10)) + "'"
+              + ", " + "'" + random_string(random.randint(1, 10)) + "'")
+
+generate_dict()
