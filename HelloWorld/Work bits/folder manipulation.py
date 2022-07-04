8680427ac80b66381b65e71355a4689c1ba85d35
Duncan R Skilton <mc03353@mbro.ac.uk>
2022-07-04 13:01:57 +0100

    added function to check for file

--- HelloWorld/Work bits/folder manipulation.py
+++ HelloWorld/Work bits/folder manipulation.py
@@ -1,4 +1,7 @@
+import csv
+import fnmatch
 import os
+import pandas as pd
 
 user_input = 0
 
@@ -51,17 +54,28 @@ def bulk_operations_menu_match():
 
 
 def create_folders_from_list():
+    select_file()
 
-    bulk_operations_menu()
+
+def select_file():
+    file_name = "student_names_and_courses.csv"
+    header = ("student name", "module name", "assignments")
+
+    for file in os.listdir("."):
+        if os.path.isfile("C:\\Users\MC03353\\PycharmProjects\\ABitOfEverything\\student_names_and_courses.csv"):
+            print("found csv files")
+            break
+    else:
+        print("file does not exist")
+
+
 
 
 def bulk_rename_folders():
-
     bulk_operations_menu()
 
 
 def bulk_rename_folders_and_sub_folders():
-
     bulk_operations_menu()
 
 
