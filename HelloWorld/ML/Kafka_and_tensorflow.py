f511d6b5bcdee1119ec38cc89a022fbc7e8bb058
DSkilton <MC03353@mbro.ac.uk>
2022-07-11 22:13:25 +0100

    added kafka and tensorflow packages to project

---
+++ HelloWorld/ML/Kafka_and_tensorflow.py
@@ -0,0 +1,14 @@
+import os
+from datetime import datetime
+import time
+import threading
+import json
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import tensorflow as tf
+import tensorflow_io as tfio
+
+print("tensorflow-io version: {}".format(tfio.__version__))
+print("tensorflow version: {}".format(tf.__version__))
