4055d4377371a8af562102ad66ce151a9e9ad4b1
DSkilton <MC03353@mbro.ac.uk>
2022-07-11 22:20:02 +0100

    refactoring FolderOperations, still needs work

--- HelloWorld/Work bits/FolderOperations.py
+++ HelloWorld/Work bits/FolderOperations.py
@@ -1,8 +1,11 @@
-import os
 import csv
+import os
+
 from win32com.client import Dispatch
 
-import Validation, MenuOptions, ViewAndChangeDirectory
+import MenuOptions
+import Validation
+import ViewAndChangeDirectory
 
 
 def create_csv_file():
@@ -10,13 +13,34 @@ def create_csv_file():
     return file
 
 
+def check_if_headers_exist():
+    print("Just checking the file...")
+    file = open("template_with_headers.csv", "r")
+
+    line = file.read().splitlines()
+
+    # while line:
+    print(line)
+
+    # this needs fixing, theows error
+    data = line.split(",")
+
+    if str(data[0]) == "Student name" and str(data[1]) == "Course name" and str(data[2]) == "Module names" and str(data[3]) == "Number of assignments":
+        print("found headers")
+
+    # line = file.readline()
+    return data
+
+
 def add_headers():
+    header = ["Student name", "Course name", "module names", "Number of assignments"]
+
     create_csv_file()
     file = create_csv_file()
 
-    # replace student name with nested list
-    header = ["Student name", "Course name", "module names", "Number of assignments"]
+    print("head" + str(check_if_headers_exist()))
 
+    # replace student name with nested list
     writer = csv.writer(create_csv_file())
     writer.writerow(header)
 
@@ -25,9 +49,8 @@ def open_excel_and_csv_file():
     excel_application = Dispatch("Excel.Application")
     excel_application.Visible = True
 
-    workbook = excel_application.Workbooks\
-        .Open(r""+ViewAndChangeDirectory.get_current_working_directory()
-        .strip()+"\\template_with_headers.csv")
+    workbook = excel_application.Workbooks.Open(r"" + ViewAndChangeDirectory.get_current_working_directory()
+                                                .strip() + "\\template_with_headers.csv")
 
 
 def add_header_to_csv_file():
@@ -54,11 +77,12 @@ def create_folders_from_list():
 
 
 def select_file():
-    file_name = "student_names_and_courses.csv"
+    file_name = "template_with_headers.csv"
     header = ("student name", "module name", "assignments")
 
     for file in os.getcwd("."):
-        if os.path.isfile(ViewAndChangeDirectory.get_current_working_directory().strip()+"\\student_names_and_courses.csv"):
+        if os.path.isfile(ViewAndChangeDirectory.get_current_working_directory()
+                                  .strip() + "\\template_with_headers.csv.csv"):
             print("found csv files, " + file)
             break
     else:
